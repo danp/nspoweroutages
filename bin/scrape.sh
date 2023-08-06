25640f0ba0ddc525b240dab51f1b4357c3ade480
Dan Peterson <danp@danp.net>
2023-08-06 15:57:18 -0300

    bin/scrape: use https

--- bin/scrape.sh
+++ bin/scrape.sh
@@ -6,8 +6,8 @@ mkdir -p data
 
 go run ./cmd/scrape-outages | jq -S 'sort_by(.id)' > data/outages.json
 
-dir="$(curl --silent --fail http://outagemap.nspower.ca/resources/data/external/interval_generation_data/metadata.json | jq -r .directory)"
-durl="http://outagemap.nspower.ca/resources/data/external/interval_generation_data/$dir"
+dir="$(curl --silent --fail https://outagemap.nspower.ca/resources/data/external/interval_generation_data/metadata.json | jq -r .directory)"
+durl="https://outagemap.nspower.ca/resources/data/external/interval_generation_data/$dir"
 
 f="report_servicearea"
 curl --silent --fail --retry 3 --max-time 15 -o - "$durl/$f.json" | jq -S . > "data/$f.json"
